1ac7cb4c59721689a7e8b4bc3bfb0140960a5af5
Brendin Barone <robotcityhq@gmail.com>
2017-07-04 20:29:29 -0500

    final calculator, can add, sub, divide, multiply, and go back to tthe starting screen

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,73 @@
 console.log('js sourced');
+
+var numX = [];
+var numY = [];
+var opperator;
+
 $(document).ready(function(){
 
-resetMath();
+  $('.numberButton').on('click', function(){
+    if(opperator) {
 
-});
+      numY.push($(this).text());
+      $('#inputField').text(numY.join(""));
+    } else {
+      numX.push($(this).text());
+      $('#inputField').text(numX.join(""));
+    }
+    console.log(numX, opperator, numY);
+  });
+  $('#clear').on('click', function() {
+    numX = [];
+    numY = [];
+    opperator= null;
+    $('#inputField').empty();
+  })
+  $('#equals').on('click', function() {
+    console.log('computing');
+    $('#answerArea').show();
+    $('.inputs').hide();
+    $.ajax({
+      type: 'POST',
+      url: '/equation',
+      data: {numX: numX, numY: numY, opperator: opperator},
+      success: function(response){
+        $('#answerArea').show();
+        $('.inputs').hide();
+        console.log(response);
+        $('#answer').text(response);
+      }
+    })
+
+  });
+
+  $('.opperatorButton').on('click', function() {
+    // numX = $('#numberX').val();
+    // numY = $('#numberY').val();
+    opperator = $(this).attr('id')
+
+  });
+
+
+
+  //reset the calc
+  resetMath();
+
+  // =button shows answer and gets rid of inputs
+  // showAnswer();
+
+}); //End of DocReady
 
 function resetMath(){
   $('#newMath').on('click', function(){
-    //Hide inputs and show difficulty settings
+    //Hide answer and restart calc settings
+    numX = [];
+    numY = [];
+    opperator= null;
     $('.inputs').show();
-    $('.answerArea').hide();
+    $('#answerArea').hide();
+    $('#inputField').empty();
+
   }
-);
-}
+)
+};
--- server/server.js
+++ server/server.js
@@ -4,6 +4,32 @@ var path = require('path');
 var port = 5002;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+var equation;
+var add;
+var subtract;
+var multiply;
+var divide;
+
+app.post('/equation', function(req, res){
+  var x = parseFloat(req.body.numX.join(""));
+  var y = parseFloat(req.body.numY.join(""));
+  var opperator = (req.body.opperator)
+  console.log(x, y, opperator);
+  add = (x + y);
+  subtract = (x - y);
+  multiply = (x * y);
+  divide = (x / y);
+  console.log(add, subtract, multiply, divide);
+  if(opperator === 'add') {
+    res.send(add.toString());
+  } else if (opperator === 'subtract'){
+    res.send(subtract.toString());
+  } else if (opperator === 'multiply'){
+    res.send(multiply.toString());
+  }else if (opperator === 'divide'){
+    res.send(divide.toString());
+  };
+});
 
 // THE CATCH ALL
 app.get('/*', function(req, res) {
